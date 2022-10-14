87036d49cda036fac8e9c976be7aca1e3e73a3c7
Julio <32122749+jcuatro@users.noreply.github.com>
2022-10-14 09:43:22 +0200

    Ejercicios Sprint 1
    Ejercicio 11 terminado

--- Sprint 1/ejercicio11/ejercicio11.ts
+++ Sprint 1/ejercicio11/ejercicio11.ts
@@ -1,23 +1,12 @@
-let gradeInput: string | null = prompt("Escribe la nonta de tu examen");
-let grade = parseInt(gradeInput);
-
-switch(grade){
-  case 0-3:
-  console.log("Muy deficiente");
-  break;
-  case 4-5:
-  console.log("Insuficiente");
-  break;
-  case 5-6:
-  console.log("Suficiente");
-  break;
-  case 6-8:
-  console.log("Bien");
-  break;
-  case 8-9:
-  console.log("Notable");
-  break;
-  case 9-10:
-  console.log("Sobresaliente");
-  break;
+const Animal = prompt('Animal');
+switch (Animal) {
+  case 'Cow':
+  case 'Giraffe':
+  case 'Dog':
+  case 'Pig':
+    console.log('This animal is not extinct.');
+    break;
+  case 'Dinosaur':
+  default:
+    console.log('This animal is extinct.');
 }
